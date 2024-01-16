4e79426522b2babd13d55e57c9e1ddeee929ef50
Lexi <lexikhecht@gmail.com>
2024-01-15 22:05:00 -0500

    set up server, added basic routes and post route

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+let newNote = require("./Develop/db/db.json");
+
+const PORT = 3002;
+// handles parsing data
+app.use(express.json());
+// set up express to handle the static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./Develop/public"));
+
+// default home route
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+);
+
+// notes route
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  let notes = fs.readFileSync(`./Develop/db/db.json`);
+  res.json(JSON.parse(notes));
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received!`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uniqid(),
+    };
+
+    let notes = fs.readFileSync(`./Develop/db/db.json`);
+    let parsedNotes = JSON.parse(notes);
+    parsedNotes.push(newNote);
+
+    fs.writeFile(`./Develop/db/db.json`, JSON.stringify(parsedNotes), (err) =>
+      err
+        ? console.error(err)
+        : console.log(`${newNote.title} note has been written to file`)
+    );
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
